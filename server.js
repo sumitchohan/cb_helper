8c3119b086746a3b9d77a8a755dd781628ca5fc1
Chohan <devsyc@ttx.com>
2022-04-30 13:11:11 -0500

    wip

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
-'use strict';
+const https = require('https');
+const fs = require('fs');
 
-const express = require('express');
+const options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
 
-// Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
-
-// App
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
-
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+https.createServer(options, function (req, res) {
+  res.writeHead(200);
+  res.end("hello world\n");
+}).listen(8080);
