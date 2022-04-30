fe1d644a39f4146d95ebe1db32d41c2ebf507026
Chohan <devsyc@ttx.com>
2022-04-30 12:10:56 -0500

    wip

--- cb.js
+++ cb.js
@@ -1,25 +1,36 @@
-var couchbase = require('couchbase')
-
-couchbase.connect(
-  'couchbase://host.docker.internal',
-  {
-    username: 'dbuser',
-    password: 'Password@123$'
-  },
-  (err, cluster) => {
-    var bucket = cluster.bucket('test')
-    var coll = bucket.defaultCollection()
-
-    newdoc = {
-      type: "hotel",
-      id: 2,
-      name: "Le Grande",
-      city: "Paris, France"
+const myArgs = process.argv.slice(2);
+var protocol="couchbase://"; 
+if(myArgs.length>1 && myArgs[1]=='s'){
+    protocol='couchbases://';
+}
+const clusterConnStr = protocol+myArgs[0];
+console.log(clusterConnStr);
+const cloudRootCertificate = 'root.pem';
+const username = 'dbuser';
+const password = 'Password@123$';
+const bucketName = 'test';
+async function go() {
+    try {
+        const cluster = await couchbase.connect(clusterConnStr, {
+            username: username,
+            password: password,
+            security: {
+                trustStorePath: cloudRootCertificate
+            }
+        });
+        const bucket = cluster.bucket(bucketName);
+        const collection = bucket.defaultCollection();
+        // Create and store a document
+        await collection.upsert('user:king_arthur', {
+            name: 'Arthur',
+            email: 'kingarthur@couchbase.com',
+            interests: ['Holy Grail', 'African Swallows']
+        });
+        let getResult = await collection.get('user:king_arthur');
+        console.log('got: ', getResult);
     }
-
-    coll.upsert('hotel_2', newdoc, (err, res) => {
-      console.log("Hotel added")
-      console.log(res)
-    })
-  }
-) 
+    catch (e) {
+        console.log(e);
+    }
+}
+go();
--- dockerfile
+++ dockerfile
@@ -9,6 +9,8 @@ WORKDIR /usr/src/app
 COPY package*.json ./
 
 RUN npm install
+
+RUN npm install couchbase --save
 # If you are building your code for production
 # RUN npm ci --only=production
 
